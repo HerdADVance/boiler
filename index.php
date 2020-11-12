63ce6649a8f9fc0afeb6facfaca457b7a6515b43
Alex <avance@bulldogcreative.com>
2020-11-11 23:46:28 -0500

    init

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="icon" href="favicon.png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=5.0" />
+
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,600,700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body class="home">
+
+    <header class="header wrap rel flex">
+       <a href="/"><img class="logo" src="/assets/images/.png" alt=""></a>
+       <nav class="nav flex-spread">
+          <ul class="flex-right">
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+          </ul>
+        </nav>
+    </header>
+
+    <main>
+      
+      <section class="home-sliders no-pad-y">
+        <div class="home-slider">
+
+        </div>
+      </section>
+
+      <section class="home-welcome wrap center">
+      </section>
+
+    </main>
+
+    <footer class="footer wrap-bg pri center">
+      <p class="no-mar-y sec-text">&copy; <?php echo date('Y'); ?> WVATA</p>
+    </footer>
+
+    <script src="/assets/js/script.js"></script>
+
+  </body>
+
+</html>
